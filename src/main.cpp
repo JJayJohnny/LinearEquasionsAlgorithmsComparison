de8eabedb91396e17bed2b6afd988269acb1c778
JJayJohnny <jasiek.barczewski@gmail.com>
2023-05-01 21:07:29 +0200

    Added Jacobi method with example

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,61 @@
 #include <iostream>
 #include <vector>
+#include <chrono>
 #include <matplotlibcpp.h>
 #include "matrix.h"
 
 #define PLOT_DIR "plots/"
+#define N 10//567 //9*7*9
+#define A1 11
+#define A2 -1
+#define A3 -1
+#define F 8
+//index 188679
 
 namespace plt = matplotlibcpp;
+
+Matrix CalculateJacobi(Matrix& A, Matrix& b, double accuracy, std::vector<int>& iterations, std::vector<double>& residuum){
+    Matrix L = A.GetTriangleL();
+    Matrix D = A.GetDiagonal();
+    Matrix U = A.GetTriangleU();
+    Matrix r(A.GetRows(), 1);
+    r.FillOnes();
+    double error = (A*r-b).CalculateNorm();
+    int i=0;
+    while(error > accuracy){
+        r = (D*(-1)).InverseDiagonalMatrix() * (L+U)*r + D.InverseDiagonalMatrix()*b;
+        error = (A*r-b).CalculateNorm();
+        i++;
+        iterations.push_back(i);
+        residuum.push_back(error);
+    }
+    return r;
+}
 
 int main(){
     Matrix m(5, 2, {{5.0, 1.0},{-3.0, 3.0},{4.0, -1.0},{-1.0, 2.0},{0.0, 7.0}});
     Matrix d(2, 7, {{1.0, 7.0, 0.0, -1.0, 9.0, 0.0, 5.0},{2.0, -6.0, -1.0, 10.0, 2.0, -3.0, 1.0}});
-    std::cout<<m.ToString()<<"\n";
-    std::cout<<(m+1).ToString()<<"\n";
+    
+
+    //zadanie A
+    Matrix A(N, N);
+    A.CreateDiagonal(A1, A2, A3);
+    Matrix b(N, 1);
+    for(int i=0; i<N; i++){
+        b.Set(i, 0, sin(i*(F+1)));
+    }
+    std::vector<double> residuum;
+    std::vector<int> iterations;
+    auto start = std::chrono::high_resolution_clock::now();
+    Matrix r = CalculateJacobi(A, b, 1e-9, iterations, residuum);
+    auto stop = std::chrono::high_resolution_clock::now();
+    std::cout<<"Metoda Jacobiego"<<"\n";
+    std::cout<<"Liczba iteracji: "<<iterations.size()<<"\n";
+    std::cout<<"Czas wykonania: "<<std::chrono::duration_cast<std::chrono::microseconds>(stop-start).count()<<"\n";
+    plt::semilogy(iterations, residuum);
+    plt::title("Wykres bledu rezydualnego dla algorytmu Jacobiego");
+    plt::xlabel("Iteracja");
+    plt::ylabel("Norma bledu rezydualnego");
+    plt::save("plots/JacobiResiduum.png");
     return 0;
 }
--- src/matrix.cpp
+++ src/matrix.cpp
@@ -1,5 +1,6 @@
 #include "matrix.h"
 #include <stdexcept>
+#include <cmath>
 
 Matrix::Matrix(int rows, int columns, std::vector<std::vector<double>> matrix){
     this->rows = rows;
@@ -19,48 +20,48 @@ void Matrix::Set(int i, int j, double value){
     this->matrix[i][j] = value;
 }
 
-double Matrix::Get(int i, int j){
+double Matrix::Get(int i, int j) const{
     return this->matrix[i][j];
 }
 
-int Matrix::GetRows(){
+int Matrix::GetRows() const{
     return this->rows;
 }
-int Matrix::GetColumns(){
+int Matrix::GetColumns() const{
     return this->columns;
 }
 
-Matrix Matrix::Add(Matrix second){
+Matrix Matrix::Add(const Matrix& b){
     Matrix result(rows, columns);
-    if(second.GetColumns() != columns || second.GetRows() != rows){
+    if(b.GetColumns() != columns || b.GetRows() != rows){
         throw std::runtime_error("Matrixes are not compatible for addition");
     }
     for(int i=0; i<rows; i++)
         for(int j=0; j<columns; j++)
-            result.Set(i, j, Get(i,j) + second.Get(i, j));
+            result.Set(i, j, Get(i,j) + b.Get(i, j));
     return result;
 }
 
-Matrix Matrix::Subtract(Matrix second){
+Matrix Matrix::Subtract(const Matrix& b){
     Matrix result(rows, columns);
-    if(second.GetColumns() != columns || second.GetRows() != rows){
+    if(b.GetColumns() != columns || b.GetRows() != rows){
         throw std::runtime_error("Matrixes are not compatible for subtraction");
     }
     for(int i=0; i<rows; i++)
         for(int j=0; j<columns; j++)
-            result.Set(i, j, matrix[i][j] - second.Get(i, j));
+            result.Set(i, j, matrix[i][j] - b.Get(i, j));
     return result;
 }
 
-Matrix Matrix::Dot(Matrix second){
-    Matrix result(rows, second.GetColumns());
-    if(columns != second.GetRows())
+Matrix Matrix::Dot(const Matrix& b){
+    Matrix result(rows, b.GetColumns());
+    if(columns != b.GetRows())
         throw std::runtime_error("Matrixes are not compatible for multiplication");
     for(int i=0; i<rows; i++){
-        for(int j=0; j<second.GetColumns(); j++){
+        for(int j=0; j<b.GetColumns(); j++){
             double sum = 0;
             for(int k=0; k<columns; k++){
-                sum+=Get(i, k)*second.Get(k, j);
+                sum+=Get(i, k)*b.Get(k, j);
             }
             result.Set(i, j, sum);
         }
@@ -117,4 +118,108 @@ Matrix Matrix::operator*(const Matrix& b){
 
 Matrix Matrix::operator*(const double& b){
     return MultiplyByScalar(b);
+}
+
+void Matrix::FillZeros(){
+    for(int i=0; i<rows; i++)
+        for(int j=0; j<columns; j++)
+            Set(i, j, 0);
+}
+
+void Matrix::FillOnes(){
+    for(int i=0; i<rows; i++)
+        for(int j=0; j<columns; j++)
+            Set(i, j, 1);
+}
+
+void Matrix::CreateDiagonal(double a1, double a2, double a3){
+    FillZeros();
+    for(int i=0; i<rows; i++){
+        Set(i, i, a1);
+        if(i>=1)
+            Set(i, i-1, a2);
+        if(i>=2)
+            Set(i, i-2, a3);
+        if(i<columns-1)
+            Set(i, i+1, a2);
+        if(i<columns-2)
+            Set(i, i+2, a3);
+    }
+}
+
+Matrix Matrix::ForwardSubstitution(const Matrix& b){
+    Matrix x(rows, 1);
+    x.FillZeros();
+    if(rows != b.GetRows())
+        throw std::runtime_error("Matrixes are not compatible for forward substitution");
+    for(int i=0; i<rows; i++){
+        double temp = b.Get(i, 0);
+        for(int j=0; j<=i; j++){
+            temp -= Get(i, j) * x.Get(j, 0);
+        }
+        x.Set(i, 0, temp / Get(i, i));
+    }
+    return x;
+}
+
+Matrix Matrix::GetTriangleL() const{
+    Matrix L(rows, columns);
+    for(int i=0; i<rows; i++){
+        for(int j=0; j<columns; j++){
+            if(j<i)
+                L.Set(i, j, Get(i, j));
+            else
+                L.Set(i, j, 0);
+        }
+    }
+    return L;
+}
+Matrix Matrix::GetTriangleU() const{
+    Matrix U(rows, columns);
+    for(int i=0; i<rows; i++){
+        for(int j=0; j<columns; j++){
+            if(j>i)
+                U.Set(i, j, Get(i, j));
+            else
+                U.Set(i, j, 0);
+        }
+    }
+    return U;
+}
+Matrix Matrix::GetDiagonal() const{
+    Matrix D(rows, columns);
+    for(int i=0; i<rows; i++){
+        for(int j=0; j<columns; j++){
+            if(j==i)
+                D.Set(i, j, Get(i, j));
+            else
+                D.Set(i, j, 0);
+        }
+    }
+    return D;
+
+}
+
+Matrix Matrix::Transpose(){
+    Matrix result(columns, rows);
+    for(int i=0; i<rows; i++){
+        for(int j=0; j<columns; j++){
+            result.Set(j, i, Get(i, j));
+        }
+    }
+    return result;
+}
+
+double Matrix::CalculateNorm(){
+    if(rows != 1 && columns != 1)
+        throw std::runtime_error("Only vectors can have norm calculated");
+    return sqrt(Transpose().Dot(*this).Get(0, 0));
+}
+
+Matrix Matrix::InverseDiagonalMatrix(){
+    Matrix result(rows, columns, matrix);
+    for(int i=0; i<rows; i++){
+        result.Set(i, i, 1/Get(i, i));
+    }
+    return result;
 }
