6c842d025cbdd7523f3a05e44e1cc3a12993beef
JJayJohnny <jasiek.barczewski@gmail.com>
2023-05-02 16:04:35 +0200

    Finished 3rd example and improved time of Jacobi method

--- src/main.cpp
+++ src/main.cpp
@@ -23,10 +23,9 @@ Matrix CalculateJacobi(Matrix& A, Matrix& b, double accuracy, std::vector<double
     double error = (A*r-b).CalculateNorm();
     double i=0;
     //these dont change through the algorithm so I preallocated them for speed
-    Matrix rParam = (D*(-1)).InverseDiagonalMatrix() * (L+U);
-    Matrix freeParam = D.InverseDiagonalMatrix()*b;
+    Matrix freeParam = D.ForwardSubstitution(b);
     while(error > accuracy){
-        r = rParam*r + freeParam;
+        r = D.ForwardSubstitution(((L+U)*r)*(-1)) + freeParam;
         error = (A*r-b).CalculateNorm();
         i++;
         iterations.push_back(i);
@@ -161,11 +160,83 @@ void Example2(){
 
 }
 
+void Example3(){
+    std::vector<double> ns = {100, 500, 1000, 2000, 3000};
+    std::vector<double> timesJacobi;
+    std::vector<double> timesGauss;
+    std::vector<double> timesLU;
+    std::vector<double> iterationsJacobi;
+    std::vector<double> iterationsGauss;
+
+    double epsilon = 1e-9;
+    for(double n : ns){
+        Matrix A(n, n);
+        A.CreateDiagonal(A1, A2, A3);
+        Matrix b(n, 1);
+        for(int i=0; i<n; i++){
+            b.Set(i, 0, sin(i*(F+1)));
+        }
+        std::vector<double> iJacobi;
+        std::vector<double> residuumJacobi;
+        auto startJacobi = std::chrono::high_resolution_clock::now();
+        Matrix xJacobi = CalculateJacobi(A, b, 1e-9, iJacobi, residuumJacobi);
+        auto stopJacobi = std::chrono::high_resolution_clock::now();
+        double timeJacobi = std::chrono::duration_cast<std::chrono::milliseconds>(stopJacobi-startJacobi).count();
+        timesJacobi.push_back(timeJacobi);
+        iterationsJacobi.push_back(iJacobi.size());
+        std::cout<<"Metoda Jacobiego N: "<<n<<" czas: "<<timeJacobi<<"ms iteracje: "<<iJacobi.size()<<"\n";
+
+        std::vector<double> iGauss;
+        std::vector<double> residuumGauss;
+        auto startGauss = std::chrono::high_resolution_clock::now();
+        Matrix xGauss = CalculateGauss(A, b, 1e-9, iGauss, residuumGauss);
+        auto stopGauss = std::chrono::high_resolution_clock::now();
+        double timeGauss = std::chrono::duration_cast<std::chrono::milliseconds>(stopGauss-startGauss).count();
+        timesGauss.push_back(timeGauss);
+        iterationsGauss.push_back(iGauss.size());
+        std::cout<<"Metoda Gaussa N: "<<n<<" czas: "<<timeGauss<<"ms iteracje: "<<iGauss.size()<<"\n";
+
+        // auto startLU = std::chrono::high_resolution_clock::now();
+        // Matrix xLU = LUFactorization(A, b);
+        // auto stopLU = std::chrono::high_resolution_clock::now();
+        // double timeLU = std::chrono::duration_cast<std::chrono::milliseconds>(stopLU-startLU).count();
+        // timesLU.push_back(timeLU);
+        // std::cout<<"Faktoryzacja LU N: "<<n<<" czas: "<<timeLU<<"\n";
+        // std::cout<<"\n";
+    }
+
+    plt::figure();
+    plt::figure_size(700, 500);
+    plt::named_plot("Metoda Jacobiego", ns, timesJacobi);
+    plt::named_plot("Metoda Gaussa-Seidla", ns, timesGauss);
+    plt::title("Czasy trwania algorytmów iteracyjnych w zależnosci od liczby niewiadomych");
+    plt::xlabel("Liczba niewiadomych");
+    plt::ylabel("Czas trwania(ms)");
+    plt::legend();
+    plt::save("plots/ZadanieECzasy.png");
+
+    // plt::named_plot("Faktoryzacja LU", ns, timesLU);
+    // plt::title("Porównanie metod iteracyjnych z bespośrednią faktoryzacją LU");
+    // plt::legend();
+    // plt::save("plots/ZadanieECzasy2.png");
+
+    plt::figure();
+    plt::named_plot("Metoda Jacobiego",ns, iterationsJacobi);
+    plt::named_plot("Metoda Gaussa-Seidla",ns, iterationsGauss); 
+    plt::title("Ilosc iteracji w zależnosci od liczby niewiadomych");
+    plt::xlabel("Liczba niewiadomych");
+    plt::ylabel("Ilosc iteracji");
+    plt::legend();
+    plt::save("plots/ZadanieEIteracje.png");  
+}
+
 int main(){
     //zadanie A i B
-    Example1();
-    //zadanie C
-    Example2();
+    //Example1();
+    //zadanie C i D
+    //Example2();
+    //zadanie E
+    Example3();
     
     return 0;
 }
